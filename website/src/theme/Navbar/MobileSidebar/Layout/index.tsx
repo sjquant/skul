a09568580034683bb5f9c4658aad4b17d0b3dbfd
Seonu Jang <seonujang92@gmail.com>
2026-05-05 00:18:47 +0900

    Align mobile navbar sidebar layout with gji website (#46)

---
+++ website/src/theme/Navbar/MobileSidebar/Layout/index.tsx
@@ -0,0 +1,58 @@
+import clsx from 'clsx';
+import React from 'react';
+import {ThemeClassNames} from '@docusaurus/theme-common';
+import {useNavbarSecondaryMenu} from '@docusaurus/theme-common/internal';
+
+type NavbarMobileSidebarLayoutProps = {
+  header: React.ReactNode;
+  primaryMenu: React.ReactNode;
+  secondaryMenu: React.ReactNode;
+};
+
+export default function NavbarMobileSidebarLayout({
+  header,
+  primaryMenu,
+  secondaryMenu,
+}: NavbarMobileSidebarLayoutProps) {
+  const {shown: secondaryMenuShown} = useNavbarSecondaryMenu();
+
+  return (
+    <div
+      className={clsx(
+        ThemeClassNames.layout.navbar.mobileSidebar.container,
+        'navbar-sidebar',
+      )}>
+      {header}
+      <div
+        className={clsx('navbar-sidebar__items', {
+          'navbar-sidebar__items--show-secondary': secondaryMenuShown,
+        })}>
+        <NavbarMobileSidebarPanel inert={secondaryMenuShown}>
+          {primaryMenu}
+        </NavbarMobileSidebarPanel>
+        <NavbarMobileSidebarPanel inert={!secondaryMenuShown}>
+          {secondaryMenu}
+        </NavbarMobileSidebarPanel>
+      </div>
+    </div>
+  );
+}
+
+function NavbarMobileSidebarPanel({
+  children,
+  inert,
+}: {
+  children: React.ReactNode;
+  inert: boolean;
+}) {
+  return (
+    <div
+      className={clsx(
+        ThemeClassNames.layout.navbar.mobileSidebar.panel,
+        'navbar-sidebar__item menu',
+      )}
+      inert={inert}>
+      {children}
+    </div>
+  );
+}
